ce80ad836297158c4a0a77c77f3f1cd63aea7a77
fraserdscott <fraserdscott@gmail.com>
2022-09-03 15:53:10 +0100

    Add lattice std-client

--- src/Example.tsx
+++ src/Example.tsx
@@ -9,6 +9,7 @@ import {
     setComponent,
     Type,
 } from "@latticexyz/recs";
+import { defineCoordComponent, defineNumberComponent } from "@latticexyz/std-client";
 import { useEffect, useState } from "react";
 
 
@@ -21,20 +22,20 @@ function Example() {
         const world = createWorld();
 
         // Define a couple components
-        const Position = defineComponent(world, { x: Type.Number, y: Type.Number });
-        const Movable = defineComponent(world, { speed: Type.Number });
+        const Position = defineCoordComponent(world);
+        const Movable = defineNumberComponent(world);
 
         defineSystem(world, [Has(Position), Has(Movable)], (update) => {
             setPosition(update.value[0]);
         });
 
-        const entity1 = createEntity(world, [withValue(Position, { x: 0, y: 0 }), withValue(Movable, { speed: 10 })]);
+        const entity1 = createEntity(world, [withValue(Position, { x: 0, y: 0 }), withValue(Movable, { value: 10 })]);
 
         setInterval(() => {
             const position = getComponentValue(Position, entity1);
             const movable = getComponentValue(Movable, entity1);
             if (position && movable) {
-                const newPosition = { x: position.x + movable.speed, y: position.y + movable.speed };
+                const newPosition = { x: position.x + movable.value, y: position.y + movable.value };
                 setComponent(Position, entity1, newPosition);
             }
         }, 1000);
