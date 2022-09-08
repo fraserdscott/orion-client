6f1300a03ceceb687f681e9cf7faae9be66b07d9
fraserdscott <fraserdscott@gmail.com>
2022-09-08 23:18:41 +0100

    Testing

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,40 +1,141 @@
 import {
-    createWorld,
-    createEntity,
-    withValue,
-    getComponentValue,
-    setComponent,
+    Component,
+    createWorld, defineComponent, getComponentEntities, getComponentValue, Schema, setComponent, Type
 } from "@latticexyz/recs";
-import { defineCoordComponent, defineNumberComponent, useComponentValueStream } from "@latticexyz/std-client";
-import { useEffect, useState } from "react";
+import { defineCoordComponent, defineStringComponent } from "@latticexyz/std-client";
+import { Contract } from "ethers";
+import { useEffect, useRef, useState } from "react";
 import { setupContracts } from "./setupContracts";
+import { abi as MoveAbi } from "./out/MoveSystem.sol/MoveSystem.json";
+import { abi as SpawnAbi } from "./out/SpawnSystem.sol/SpawnSystem.json";
+import { createDecoder } from "@latticexyz/network";
+import ComponentAbi from "@latticexyz/solecs/abi/Component.json";
+import { Component as SolecsComponent } from "@latticexyz/solecs";
 
+const ZOOM = 100;
+const PRECISION = 10 ** 18;
+
+const MOVE_ADDRESS = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+const SPAWN_ADDRESS = "0x5fc8d32690cc91d4c39d9d3abcbd16989f875707";
+
+const keyToDirection: Record<string, number> = { w: 0, d: 1, s: 2, a: 3 };
+
+const start = async () => {
+    const world = createWorld();
+    const Components = {
+        Position: defineCoordComponent(world, { id: "Position", metadata: { contractId: "example.component.Position" } }),
+        Square: defineComponent(world, { x: Type.Number, y: Type.Number, width: Type.Number, height: Type.Number }, { id: "Square", metadata: { contractId: "example.component.Square" } }),
+        SystemsRegistry: defineStringComponent(world, {
+            id: "SystemsRegistry",
+            metadata: { contractId: "world.component.systems" },
+        }),
+        ComponentsRegistry: defineStringComponent(world, {
+            id: "ComponentsRegistry",
+            metadata: { contractId: "world.component.components" },
+        })
+    };
+
+    const s = await setupContracts(world, Components);
+    const filter = s.contracts.get().World.filters.ComponentValueSet();
+    s.network.providers.get().json.on(filter, c => {
+        console.log(c);
+
+        const component = c.topics[1];
+        const entity = c.topics[3];
+        const componentKey = s.mappings[component];
+
+        s.contracts.get().World.getComponent(component).then(address => {
+            const contract = new Contract(address, ComponentAbi.abi, s.network.providers.get().json) as SolecsComponent;
+
+            contract.getSchema().then(([keys, values]) => {
+                contract.getRawValue(entity).then(data => {
+                    const decoder = createDecoder(
+                        keys,
+                        values
+                    );
+
+                    const decoded = decoder(data);
+                    setComponent(Components[componentKey] as Component<Schema>, world.entityToIndex.get(entity) ?? world.registerEntity({ id: entity }), decoded);
+                });
+            })
+        });
+    });
+
+    return {
+        setup: s,
+        Components
+    };
+}
 
 function Example() {
-    const [world] = useState(createWorld());
-    const [Position] = useState(defineCoordComponent(world));
-    const [Movable] = useState(defineNumberComponent(world));
-    const positionStream = useComponentValueStream(Position, 0);
+    const [stuff, setStuff] = useState<any>({});
+    const canvasRef = useRef(null);
 
     useEffect(() => {
-        setupContracts(world).then(r => console.log(r));
+        start().then(st => {
+            setStuff(st);
+        })
+    }, []);
 
-        const entity1 = createEntity(world, [withValue(Position, { x: 0, y: 0 }), withValue(Movable, { value: 10 })]);
+    useEffect(() => {
+        if (stuff && stuff.Components) {
+            const canvas: any = canvasRef.current;
+            const context = canvas.getContext("2d");
 
-        setInterval(() => {
-            const position = getComponentValue(Position, entity1);
-            const movable = getComponentValue(Movable, entity1);
-            if (position && movable) {
-                const newPosition = { x: position.x + movable.value, y: position.y + movable.value };
-                setComponent(Position, entity1, newPosition);
+            // BACKGROUND
+            context.fillStyle = "#000000";
+            context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+
+            // OBJECTS
+            for (const entity of getComponentEntities(stuff.Components.Position)) {
+                const p = getComponentValue(stuff.Components.Position, entity);
+
+                context.fillStyle = "#FFFFFF";
+                context.fillRect(
+                    (p.x / PRECISION - 0.5) * ZOOM + context.canvas.width / 2,
+                    context.canvas.height - (p.y / PRECISION + 0.5) * ZOOM - context.canvas.height / 2,
+                    1,
+                    1
+                )
+            };
+            for (const entity of getComponentEntities(stuff.Components.Square)) {
+                const s = getComponentValue(stuff.Components.Square, entity);
+
+                context.fillStyle = "#FFFFFF";
+                context.fillRect(
+                    (s.x / PRECISION - 0.5) * ZOOM + context.canvas.width / 2,
+                    context.canvas.height - (s.y / PRECISION + 0.5) * ZOOM - context.canvas.height / 2,
+                    s.width / PRECISION * ZOOM,
+                    s.height / PRECISION * ZOOM
+                )
+
             }
-        }, 1000);
-    }, [])
+        }
+    }, [stuff]);
+
+    useEffect(() => {
+        if (stuff.setup) {
+            window.addEventListener("keydown", e => {
+                if (e.key in keyToDirection) {
+                    const contract = new Contract(MOVE_ADDRESS, MoveAbi, stuff.setup.network.signer.get());
+
+                    contract.executeTyped(keyToDirection[e.key]);
+                }
+            });
+        }
+    }, [stuff.setup]);
 
     return (
         <div className="App">
-            <div>{positionStream?.x}</div>
-        </div>
+            <canvas
+                ref={canvasRef}
+            />
+            <button onClick={() => {
+                const contract = new Contract(SPAWN_ADDRESS, SpawnAbi, stuff.setup.network.signer.get());
+
+                contract.execute([]);
+            }}>Spawn</button>
+        </div >
     )
 }
 
--- src/config.ts
+++ src/config.ts
@@ -15,5 +15,6 @@ export const config: NetworkConfig = {
       skipNetworkCheck: true,
     },
   },
-  chainId: 100,
+  privateKey: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+  chainId: 31337,
 };
